6dd6bb602892848888a9bcb250ac7f335cb17e99
Bruno Granado <bruno.granado@unesp.br>
2025-10-23 21:21:49 -0300

    implement: Filter By HealthPlans in GetDoctorsByFilter

--- medical-appointment-scheduling-api/Repository/DoctorsRepository.cs
+++ medical-appointment-scheduling-api/Repository/DoctorsRepository.cs
@@ -10,10 +10,11 @@ namespace medical_appointment_scheduling_api.Repositories
     public class DoctorsRepository : IDoctorsRepository
     {
         private readonly AppDbContext _db;
-
-        public DoctorsRepository(AppDbContext context)
+        private readonly UserData _userData;
+        public DoctorsRepository(UserData userData, AppDbContext context)
         {
             _db = context;
+            _userData = userData;
         }
 
         public async Task<IEnumerable<Doctors>> GetAllAsync()
@@ -53,8 +54,27 @@ namespace medical_appointment_scheduling_api.Repositories
 
         public async Task<List<Doctors>> GetDoctorsByFilter(FiltroMedicos filtro)
         {
-            var medicos = await _db.Doctors.Where(w => w.Specialty == filtro.specialty).ToListAsync();
-            return medicos;
+            var qmedicos = _db.Doctors.AsQueryable();
+
+            if (filtro.specialty != 0)
+            {
+                qmedicos = qmedicos.Where(w => w.Specialty == filtro.specialty);
+            }
+
+            if (filtro.FilterByHealthPlans)
+            {
+                var userHealthPlans = await _db.ClientHealthPlans
+                    .Where(c => c.ClientId == _userData.ClientId)
+                    .Select(s => s.HealthPlanId).ToListAsync();
+
+                qmedicos =
+                    (from d in qmedicos
+                     join hp in _db.DoctorHealthPlans on d.Id equals hp.DoctorId
+                     where userHealthPlans.Contains(hp.HealthPlanId)
+                     select d).AsQueryable();
+            }
+
+            return await qmedicos.ToListAsync();
         }
     }
 }
