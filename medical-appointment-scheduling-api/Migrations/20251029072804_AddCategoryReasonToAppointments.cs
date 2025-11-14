86a19bd432d2543723b23ace4bca88fe33fa3926
dppereira <diogoppereira504@gmail.com>
2025-11-14 18:59:20 -0300

    feat(appointments): add category, reason and canceled_at columns to table

---
+++ medical-appointment-scheduling-api/Migrations/20251029072804_AddCategoryReasonToAppointments.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace medical_appointment_scheduling_api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCategoryReasonToAppointments : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "canceled_at",
+                table: "Appointments",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<short>(
+                name: "category",
+                table: "Appointments",
+                type: "smallint",
+                nullable: false,
+                defaultValue: (short)0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "reason",
+                table: "Appointments",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "canceled_at",
+                table: "Appointments");
+
+            migrationBuilder.DropColumn(
+                name: "category",
+                table: "Appointments");
+
+            migrationBuilder.DropColumn(
+                name: "reason",
+                table: "Appointments");
+        }
+    }
+}
